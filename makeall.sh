f16f5c1b582668d0eb4d47ee7ea2a4507756267c
superhaowei <32660262+superhaowei@users.noreply.github.com>
2018-09-07 15:21:14 +0800

    Update makeall.sh

--- makeall.sh
+++ makeall.sh
@@ -1,11 +1,12 @@
 #add by haowei.cheng
 make distclean
-rm /home/haowei/samba_share/u-boot.bin -rf
-rm /home/haowei/samba_share/u-boot.s -rf
+#rm /home/haowei/samba_share/u-boot.bin -rf
+#rm /home/haowei/samba_share/u-boot.s -rf
 make stm32f746-discovery_config
 make
-echo "cp bin start..."
-cp u-boot.bin /home/haowei/samba_share/
-echo "cp bin end!"
+#echo "cp bin start..."
+#cp u-boot.bin /home/haowei/samba_share/
+#echo "cp bin end!"
+#for debug
 arm-none-eabi-objdump -d u-boot>u-boot.s
-cp u-boot.s /home/haowei/samba_share/
+#cp u-boot.s /home/haowei/samba_share/
